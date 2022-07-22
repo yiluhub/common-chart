7ff83b957e307cc830b97c9b67c3cc87942446d0
Ugur Guneri <ugur@simpletrip.de>
2022-07-22 15:35:57 +0200

    YAF-1926-fix-publish-script fixed publish script (#17)
    add master branch checkout
    
    use current branch for push
    
    use TRAVIS_BRANCH env variable for branch name
    
    add check for existing release to break the loop
    
    Co-authored-by: ugur.guneri <ugur.guneri@yiluhub.com>

--- scripts/publish_chart.sh
+++ scripts/publish_chart.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
 VERSION=$(grep 'version: [0-9]\+\.[0-9]\+\.[0-9]\+' "yilu-common/Chart.yaml" | cut -d':' -f2 | tr -d '[:space:]')
+TRAVIS_GITHUB_TOKEN_USERNAME="Travis-CI"
+# from travis-ci
+BRANCH_NAME=$TRAVIS_BRANCH
+LAST_COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE
 
-if echo "${VERSION}" | grep -Eq "^v[0-9]+(\.[0-9]+){2}$"; then
-    REPOSITORY="https://$TRAVIS_GITHUB_TOKEN@raw.githubusercontent.com/yiluhub/common-chart.git"
-    git config user.email yilu-bot@users.noreply.github.com
-    git config user.name yilu-bot
+if echo "${VERSION}" | grep -Eq "^[0-9]+(\.[0-9]+){2}$"; then
+  if echo "${LAST_COMMIT_MESSAGE}" | grep -Eq "version [0-9]+(\.[0-9]+){2} has been created"; then
+    echo "Release VERSION already created! Skipping release generation"
+    exit 0
+  else
+    REPOSITORY="https://$TRAVIS_GITHUB_TOKEN_USERNAME:$TRAVIS_GITHUB_TOKEN@github.com/yiluhub/common-chart.git"
+    git config user.email $TRAVIS_GITHUB_TOKEN_USERNAME@users.noreply.github.com
+    git config user.name $TRAVIS_GITHUB_TOKEN_USERNAME
     git remote set-url origin "${REPOSITORY}"
+    echo "✅ Set remote origin to $REPOSITORY"
+    git checkout "$BRANCH_NAME"
+    echo "✅ Checking out to $BRANCH_NAME"
     helm package yilu-common --dependency-update
     mv yilu-common-"$VERSION".tgz docs
     helm repo index docs --url https://yiluhub.github.io/common-chart/
-    cp yilu-common/README.MD docs/
-    git add yilu-common
+    cp yilu-common/README.md docs/
+    git add docs/
     git commit -m "version $VERSION has been created"
-    git push origin master
+    git push origin "$BRANCH_NAME"
     echo "✅ Published charts"
+  fi
 else
     echo "Not a valid semver release tag! Skip charts package"
     exit 0
