6292fee18ad941e96179a363978070c2c4917ea3
David Mukiibi <david.mukiibi@yiluhub.com>
2024-04-03 15:55:11 +0200

    remove github user since it is not used anymore

--- scripts/publish_chart.sh
+++ scripts/publish_chart.sh
@@ -2,7 +2,6 @@
 set -eo pipefail
 
 VERSION=$(grep 'version: [0-9]\+\.[0-9]\+\.[0-9]\+' "yilu-common/Chart.yaml" | cut -d':' -f2 | tr -d '[:space:]')
-TRAVIS_GITHUB_TOKEN_USERNAME="yilu-travis-ci"
 # from the CI
 BRANCH_NAME=$CI_BRANCH
 LAST_COMMIT_MESSAGE=$CI_COMMIT_MESSAGE
@@ -12,9 +11,7 @@ if echo "${VERSION}" | grep -Eq "^[0-9]+(\.[0-9]+){2}$"; then
     echo "Release VERSION already created! Skipping release generation"
     exit 0
   else
-    REPOSITORY="https://$TRAVIS_GITHUB_TOKEN_USERNAME:$TRAVIS_GITHUB_TOKEN@github.com/yiluhub/common-chart.git"
-    git config user.email $TRAVIS_GITHUB_TOKEN_USERNAME@users.noreply.github.com
-    git config user.name $TRAVIS_GITHUB_TOKEN_USERNAME
+    REPOSITORY="https://github.com/yiluhub/common-chart.git"
     git remote set-url origin "${REPOSITORY}"
     echo "✅ Set remote origin to $REPOSITORY"
     git checkout "$BRANCH_NAME"
